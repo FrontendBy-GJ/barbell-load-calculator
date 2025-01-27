4ab29cf8d218e899cf6f19804777619c86abb7b4
Genaro Garcia <genaro.dev@outlook.com>
2025-01-27 10:56:40 -0600

    style: remove abbr element underline

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,6 +8,7 @@ export default function Header() {
         <img
           src={barbell}
           width="300"
+          height="300"
           alt="Barbell Load Calculator Logo"
           className="mx-auto h-24 object-contain"
           loading="lazy"
@@ -15,7 +16,7 @@ export default function Header() {
         <h1 className="acronym absolute left-0 right-0 top-1/2 mx-auto w-fit -translate-y-1/2 bg-[var(--bg-clr)] px-2 py-1 text-6xl tracking-wide text-blue-600">
           <abbr
             title="Barbell Load Calculator"
-            className="relative before:absolute before:-top-0 before:left-1/2 before:right-1/2 before:h-2 before:w-[110px] before:-translate-x-1/2 before:rounded before:bg-current after:absolute after:-bottom-2 after:left-1/2 after:right-1/2 after:h-2 after:w-[110px] after:-translate-x-1/2 after:rounded after:bg-current"
+            className="relative no-underline before:absolute before:-top-0 before:left-1/2 before:right-1/2 before:h-2 before:w-[110px] before:-translate-x-1/2 before:rounded before:bg-current after:absolute after:-bottom-0 after:left-1/2 after:right-1/2 after:h-2 after:w-[110px] after:-translate-x-1/2 after:rounded after:bg-current"
           >
             BLC
           </abbr>
