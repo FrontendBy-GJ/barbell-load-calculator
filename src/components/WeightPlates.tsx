e6b8ec2db39700077c0e808d4c58b5166a282909
Genaro Garcia <genaro.dev@outlook.com>
2025-07-09 12:42:25 -0500

    style: improve layout and alignment in WeightPlates component for better readability

--- src/components/WeightPlates.tsx
+++ src/components/WeightPlates.tsx
@@ -2,7 +2,7 @@ import useWeightPlatesContext from "../hooks/useWeightPlatesContext";
 import WeightPlate from "./weight-plate/WeightPlate";
 
 export default function WeightPlates() {
-  let { platesAvailable, platesNeeded, targetWeight } =
+  const { platesAvailable, platesNeeded, targetWeight } =
     useWeightPlatesContext();
 
   const totalWeightOfPlatesAvail = Object.entries(platesAvailable).reduce(
@@ -11,17 +11,17 @@ export default function WeightPlates() {
   );
 
   return (
-    <div className="my-auto flex h-full w-full flex-col">
+    <div className="my-auto flex h-full w-full flex-col items-center justify-center">
       {targetWeight ? (
-        <span className="font-Bebas text-center text-5xl md:text-6xl">
+        <span className="mt-4 text-center font-Bebas text-5xl md:text-6xl">
           {targetWeight} lbs
         </span>
       ) : (
-        <p className="font-Bebas mt-4 text-center text-5xl md:text-6xl">
+        <p className="mt-4 text-center font-Bebas text-5xl md:text-6xl">
           Available: {totalWeightOfPlatesAvail}lbs
         </p>
       )}
-      <div className="flex flex-wrap items-center justify-center -space-y-16 px-8 py-10 lg:-space-x-16 lg:space-y-0 lg:px-12">
+      <div className="flex flex-wrap items-center justify-center -space-y-16 px-8 py-10 lg:-space-x-16 lg:space-y-0 lg:px-20">
         {platesNeeded.size === 0 &&
           Object.entries(platesAvailable)
             .sort(([a], [b]) => Number(b) - Number(a))
