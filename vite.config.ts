565eb9b360e441e67183da2f14ae57e5ddd4b4a8
Genaro Garcia <genaro.dev@outlook.com>
2025-08-07 14:29:00 -0500

    feat: add PWA support with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Barbell Load Calculator",
+        short_name: "BLC",
+        description:
+          "The Barbell Load Calculator (BLC) helps you easily calculate the exact weight plates you need to load on your barbell for your desired lift weight. Perfect for strength training and weightlifting enthusiasts.",
+        theme_color: "#2463EB",
+        background_color: "#f1f5fa",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
