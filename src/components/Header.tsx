b80be40922b93a7d053e333052916481809ae44e
Genaro Garcia <genaro.dev@outlook.com>
2025-09-08 11:00:08 -0500

    fix: update header class references for consistency in styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/blc-logo.svg";
 
 export default function Header() {
   return (
-    <header role="banner" className="mx-auto w-full max-w-7xl">
+    <header role="banner" className="header mx-auto w-full max-w-7xl">
       <h1 className="mx-auto w-fit">
         <img src={logo} alt="Barbell Load Calculator" width={320} height={60} />
       </h1>
